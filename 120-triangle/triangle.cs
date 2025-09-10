c4a0f18fbf9758e8a6f572fcf20c83eabef708ed
Stanislav <donotcare16@gmail.com>
2025-09-10 17:37:11 +0300

    Time: 3 ms (77.35%) | Memory: 44 MB (82.06%) - LeetSync

--- 120-triangle/triangle.cs
+++ 120-triangle/triangle.cs
@@ -7,15 +7,8 @@ public class Solution {
         for (int i = 0; i < dp.Length; i++) dp[i] = triangle[lastRowIndex][i];
 
         for (int row = lastRowIndex - 1; row >= 0; row--)
-        {
-            int cols = triangle[row].Count;
-            int[] newDp = new int[cols];
-            for (int col = 0; col < cols; col++)
-            {
-                newDp[col] = triangle[row][col] + Math.Min(dp[col], dp[col + 1]);
-            }
-            dp = newDp;
-        }
+            for (int col = 0; col < triangle[row].Count; col++)
+                dp[col] = triangle[row][col] + Math.Min(dp[col], dp[col + 1]);
 
         return dp[0];
     }
